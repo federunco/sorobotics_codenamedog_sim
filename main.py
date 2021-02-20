ac7bd63618a14d60d36494df50d60aea6209324d
Federico Runco <federico.runco@gmail.com>
2021-02-20 23:27:47 +0100

    Home position with g implemented

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import math
 from ik.ik import IKSolver
 from ik.leg import Leg
 
-GRAVITY = 0#-9.81 # non facciamo gli ingegneri va
+GRAVITY = -9.81 # non facciamo gli ingegneri va
 dt = 1e-3
 iters = 2000
 exc_path = pathlib.Path(__file__).parent.absolute()
@@ -31,6 +31,14 @@ for joint in range(p.getNumJoints(botID)):
         joints_index[joint_info[1].decode("utf-8")] = joint
 
 print(joints_index)
+
+def move_leg(leg_name, alpha, beta, gamma, spd):
+    p.setJointMotorControl2(botID, joints_index[leg_name + '_shoulder'], p.POSITION_CONTROL, targetPosition=gamma-math.pi/2, force=1000, maxVelocity=spd)
+    p.setJointMotorControl2(botID, joints_index[leg_name + '_leg'], p.POSITION_CONTROL, targetPosition=beta-math.pi/2, force=1000, maxVelocity=spd)
+    p.setJointMotorControl2(botID, joints_index[leg_name + '_foot'], p.POSITION_CONTROL, targetPosition=alpha-math.pi, force=1000, maxVelocity=spd)
+
+
+
 """
 jointFrictionForce = 1
 
@@ -49,19 +57,17 @@ p.changeDynamics(botID, joints_index['rear_right_foot'], lateralFriction=2)
 # set base position
 
 ik = IKSolver(simulated_environment=True)
-l_fl = Leg()
-l_fr = Leg(right_axis=True)
-l_bl = Leg(back_axis=True)
-l_br = Leg(back_axis=True, right_axis=True)
-alpha, beta, gamma = ik.solve(l_fl, 0, 5.5, 15)
-print(gamma)
-print(beta)
-print(alpha)
+leg_l = Leg()
+leg_r = Leg(right_axis=True)
+alpha_l, beta_l, gamma_l = ik.solve(leg_l, 0, 5.5, 15)
+alpha_r, beta_r, gamma_r = ik.solve(leg_l, 0, 5.5, 15)
+
 spd=1
 
-p.setJointMotorControl2(botID, joints_index['front_left_shoulder'], p.POSITION_CONTROL, targetPosition=gamma-math.pi/2, force=1000, maxVelocity=spd)
-p.setJointMotorControl2(botID, joints_index['front_left_leg'], p.POSITION_CONTROL, targetPosition=beta- math.pi/2, force=1000, maxVelocity=spd)
-p.setJointMotorControl2(botID, joints_index['front_left_foot'], p.POSITION_CONTROL, targetPosition=alpha-math.pi, force=1000, maxVelocity=spd)
+move_leg('front_left', alpha_l, beta_l, gamma_l, spd)
+move_leg('rear_left', alpha_l, beta_l, gamma_l, spd)
+move_leg('front_right', alpha_r, beta_r, gamma_r, spd)
+move_leg('rear_right', alpha_r, beta_r, gamma_r, spd)
 
 p.setRealTimeSimulation(1)
 while(1):
